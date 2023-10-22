9d46ba63f41f35ffbb739da727ff6771e1d9671c
Alexeykaa <ka.alexey@gmail.com>
2023-10-22 22:21:05 +0300

    init implementation

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,33 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        Queue<Integer> currentQueue = new LinkedList<>(firstQueue);
+        Queue<Integer> nextQueue = new LinkedList<>(secondQueue);
+
+        addTwoCards(result, currentQueue);
+        addTwoCards(result, nextQueue);
+        while (!currentQueue.isEmpty()) {
+            currentQueue.add(result.pollLast());
+            addTwoCards(result, currentQueue);
+
+            // swap players
+            Queue<Integer> temp = currentQueue;
+            currentQueue = nextQueue;
+            nextQueue = temp;
+        }
+        return result;
+    }
+
+    private void addTwoCards(ArrayDeque<Integer> result, Queue<Integer> queue) {
+        result.add(queue.poll());
+        result.add(queue.poll());
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,21 @@
 package com.epam.collections.queue;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        LinkedList<Integer> dishes = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+        LinkedList<Integer> result = new LinkedList<>();
+        int i = 0;
+        int step = everyDishNumberToEat - 1;
+        while (!dishes.isEmpty()) {
+            i = (i + step) % dishes.size();
+            result.add(dishes.remove(i));
+        }
+        return result;
     }
 }
